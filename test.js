74c62a48606c4c8b9964854bc103a73118dc24d8
jhon escobar <jhonmiltonescobarsaavedra@gamil.com>
2026-08-13 00:13:09 -0400

    test: provocar fallo intencional

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 console.log('Ejecutando pruebas de TaskNote...');
 const fs = require('fs');
-if (!fs.existsSync('./index.html')) {
+if (!fs.existsSync('./indesadsadasx.html')) {
  console.error('FALLO: index.html no encontrado');
  process.exit(1);
 }
